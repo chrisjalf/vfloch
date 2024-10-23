0755fd732f09b7fc93bd46b9de675d4a88b80d64
Chris James <xcastroj97@gmail.com>
2024-10-23 20:30:39 +0800

    add not found page and route

---
+++ src/pages/NotFound.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="not-found text-center">
+    <h1 class="">404 Not Found</h1>
+    <div>
+      <div class="spinner-grow text-danger" role="status">
+        <span class="visually-hidden">Loading...</span>
+      </div>
+
+      <div class="spinner-grow text-danger" role="status">
+        <span class="visually-hidden">Loading...</span>
+      </div>
+
+      <div class="spinner-grow text-danger" role="status">
+        <span class="visually-hidden">Loading...</span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.not-found {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: calc(100vh - 110px);
+}
+
+.spinner-grow {
+  margin: 0 0.5rem;
+}
+</style>
--- src/router.js
+++ src/router.js
@@ -1,10 +1,14 @@
 import { createRouter, createWebHistory } from "vue-router";
 
 const Home = () => import("./pages/Home.vue");
+const NotFound = () => import("./pages/NotFound.vue");
 
 const router = createRouter({
   history: createWebHistory(),
-  routes: [{ path: "/", component: Home }],
+  routes: [
+    { path: "/", component: Home },
+    { path: "/:notFound(.*)", component: NotFound },
+  ],
 });
 
 export default router;
